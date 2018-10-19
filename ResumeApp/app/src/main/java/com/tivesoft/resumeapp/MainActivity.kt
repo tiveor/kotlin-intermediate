326808f13eb1e37f31eb30ca9244a8e2d2a9919a
Alvaro Orellana <tiveor@gmail.com>
2018-10-19 14:37:41 -0400

    ResumeApp Intent

--- ResumeApp/app/src/main/java/com/tivesoft/resumeapp/MainActivity.kt
+++ ResumeApp/app/src/main/java/com/tivesoft/resumeapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.tivesoft.resumeapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnWorkHistory = findViewById<Button>(R.id.btnWorkHistory)
+
+
+        btnWorkHistory.setOnClickListener {
+
+            var intent = Intent(applicationContext, WorkHistoryActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
