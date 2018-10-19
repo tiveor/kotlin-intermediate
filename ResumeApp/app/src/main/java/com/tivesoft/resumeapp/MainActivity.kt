cae1642da8f67a988454deb8bd954de95ba50177
Alvaro Orellana <tiveor@gmail.com>
2018-10-19 18:21:49 -0400

    ResumeApp Call and Email

--- ResumeApp/app/src/main/java/com/tivesoft/resumeapp/MainActivity.kt
+++ ResumeApp/app/src/main/java/com/tivesoft/resumeapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tivesoft.resumeapp
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,13 +13,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var btnWorkHistory = findViewById<Button>(R.id.btnWorkHistory)
-
-
         btnWorkHistory.setOnClickListener {
 
             var intent = Intent(applicationContext, WorkHistoryActivity::class.java)
             startActivity(intent)
         }
+
+        var btnCallMe = findViewById<Button>(R.id.btnCallMe)
+        btnCallMe.setOnClickListener {
+            var phoneUri = Uri.parse("tel:+59112345678")
+            var intent = Intent(Intent.ACTION_DIAL, phoneUri)
+            startActivity(intent)
+        }
+
+        var btnEmailMe = findViewById<Button>(R.id.btnEmailMe)
+        btnEmailMe.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SEND)
+
+            val to = "test@gmail.com"
+            val subject = "Test Email Subject"
+            val message = "Test Message"
+
+//            intent.setType("message/rfc822")
+            intent.setType("plain/text")
+            intent.putExtra(Intent.EXTRA_EMAIL, to)
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+
+            startActivity(intent);
+        }
+
 
     }
 }
